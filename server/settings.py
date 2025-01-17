b4252cca3b71df96813faf0c8df1370db3ce7ec3
Siddhant <siddhant_010@outlook.com>
2025-01-17 11:55:31 +0530

    new2

--- server/settings.py
+++ server/settings.py
@@ -211,7 +211,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'your-development-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['*']  # Avoid this unless absolutely necessary!
+
 
 # Application definition
 
